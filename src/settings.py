eed0837b23e40e63fa3b1a9aa4018470b74a977a
Walker Waylon Scott <walker_waylon_s@cat.com>
2017-06-26 19:34:03 -0500

    feat(settings) Initial Commit of settings
    Sets up directory structures.

---
+++ src/settings.py
@@ -0,0 +1,23 @@
+
+import os
+import sys
+from matplotlib import rcParams
+
+try:
+    root_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+except NameError:
+    root_dir = os.path.abspath(os.path.dirname(_dh[0]))
+except NameError:
+    root_dir = os.path.abspath(os.path.dirname(os.getcwd))
+
+reports_dir = os.path.join(root_dir, 'reports')
+data_dir = os.path.join(root_dir, 'data')
+raw_data_dir = os.path.join(root_dir, 'data', 'raw')
+processed_data_dir = os.path.join(root_dir, 'data', 'processed')
+src_dir = os.path.join(root_dir, 'src')
+analysis_dir = os.path.dirname(root_dir)
+static_dir = os.path.join(src_dir, 'static')
+
+sys.path.append(analysis_dir)
+sys.path.append(root_dir)
+sys.path.append(src_dir)
