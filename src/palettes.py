f5e4b5f006e5f5508c686ed67eedf92daa224cf7
Walker Waylon Scott <walker_waylon_s@cat.com>
2017-07-02 11:33:51 -0500

    feat(color palettes)  Added a color palettes file

---
+++ src/palettes.py
@@ -0,0 +1,13 @@
+
+# http://www.pictaculous.com/ from data viz day image
+dvd = ['#6998A6', '#966001', '#08414C', '#B80000']
+
+# http://www.colourlovers.com/palette/994858/You_helped_me.
+you_helped_me = ['#004444', '#BB0832', '#43ABCE', '#4D9913', '#423D37', '#004444', '#BB0832', '#43ABCE']
+
+# http://www.colourlovers.com/palette/49613/todays_outfit
+todays_outfit = ['#EC1616', '#F0DC9D', '#006CF8', '#FFFFFF', '#000000', '#EC1616', '#F0DC9D', '#006CF8']
+
+# http://www.colourlovers.com/palette/2097539/Sea_Urchin_Spines
+sea_urchin_spines = ['#000E29', '#0071A7', '#24A854', '#057976', '#004444', '#000E29', '#0071A7', '#24A854']
+# https://color.adobe.com/index.cfm#themeID/937271
