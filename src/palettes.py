54e187c83ef01f2dcee9b6e0caca62e23d84392a
walkews <walker_waylon_s@cat.com>
2017-07-11 08:26:57 -0500

    removed white from palette

--- src/palettes.py
+++ src/palettes.py
@@ -6,7 +6,7 @@ dvd = ['#6998A6', '#966001', '#08414C', '#B80000']
 you_helped_me = ['#004444', '#BB0832', '#43ABCE', '#4D9913', '#423D37', '#004444', '#BB0832', '#43ABCE']
 
 # http://www.colourlovers.com/palette/49613/todays_outfit
-todays_outfit = ['#EC1616', '#F0DC9D', '#006CF8', '#FFFFFF', '#000000', '#EC1616', '#F0DC9D', '#006CF8']
+todays_outfit = ['#EC1616', '#F0DC9D', '#006CF8', '#000000', '#EC1616', '#F0DC9D', '#006CF8']
 
 # http://www.colourlovers.com/palette/2097539/Sea_Urchin_Spines
 sea_urchin_spines = ['#000E29', '#0071A7', '#24A854', '#057976', '#004444', '#000E29', '#0071A7', '#24A854']
